e1aefc9b2d182cb8e028c9228c1bd41eeb9f9657
manyoch <gmatnyoch@gmail.com>
2023-02-19 11:16:10 -0800

    5-printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - enrty piont
+ * Return: 0 always (success)
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
