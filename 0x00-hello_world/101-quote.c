28523726b5b3b0853e30677ca62b6a525dc58d33
manyoch <gmatnyoch@gmail.com>
2023-02-19 14:39:44 -0800

    101-quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - A program that pritns standard error
+ * Return: 1 (success)
+ */
+int main(void)
+{
+	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19", stdout);
+	return (1);
+}
