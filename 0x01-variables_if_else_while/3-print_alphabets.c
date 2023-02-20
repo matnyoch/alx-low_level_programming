5cddbcc4664b271aa89f0be68bfe5b917b264a34
manyoch <gmatnyoch@gmail.com>
2023-02-20 15:45:10 -0800

    3-print_alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry point
+ * Return: 0 always (success)
+ */
+int main(void)
+{
+	int ch = 'a';
+	int CH = 'A';
+
+	while (ch = 'a')
+	{
+
+		putchar(ch);
+	}
+	while (CH = 'A')
+	{
+
+		putchar(CH);
+	}
+	putchar('\n');
+	return (0);
+}
